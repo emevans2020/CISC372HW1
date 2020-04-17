ec6e9e579405fd5f37b430dde9abe134871ea31f
Emily Evans <42969703+emevans2020@users.noreply.github.com>
2020-04-16 23:32:28 -0400

    Add files via upload

---
+++ hw1/myhi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int i=0;
+   while (i<10){
+   	printf("Hello from Emily!\n");
+	printf("word here\n");
+	i++;
+   }
+   return 0;
+}
